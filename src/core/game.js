7d6f5a344d19951bde25afa8c1f330dfabdae1ce
Isuru Karunaratna <93091809+isuruK2003@users.noreply.github.com>
2025-06-08 00:44:29 +0530

    refactor: inline renderFrame method into setRenderFrame for improved readability

--- src/core/game.js
+++ src/core/game.js
@@ -1,38 +1,36 @@
 export class GameOfLife {
     constructor(canvasGrid) {
         this.canvasGrid = canvasGrid;
-        this.canvasGrid.setRenderFrame(() => this.renderFrame());
-    }
-
-    renderFrame() {
-        const currentGrid = this.canvasGrid.cellGrid;
-        const newGrid = this.canvasGrid.cellGrid.getClone();
-        for (let i = 0; i < currentGrid.rows * currentGrid.cols; i++) {
-            const x = i % currentGrid.cols;
-            const y = Math.floor(i / currentGrid.cols);
-
-            let n = 0;
-            for (let dx of [-1, 0, 1]) {
-                for (let dy of [-1, 0, 1]) {
-                    if (dx === 0 && dy === 0) continue;
-                    const nx = (x + dx + currentGrid.cols) % currentGrid.cols;
-                    const ny = (y + dy + currentGrid.rows) % currentGrid.rows;
-                    if (currentGrid.getCellState(nx, ny) === 1) n++;
+        this.canvasGrid.setRenderFrame(() => {
+            const currentGrid = this.canvasGrid.cellGrid;
+            const newGrid = this.canvasGrid.cellGrid.getClone();
+            for (let i = 0; i < currentGrid.rows * currentGrid.cols; i++) {
+                const x = i % currentGrid.cols;
+                const y = Math.floor(i / currentGrid.cols);
+        
+                let n = 0;
+                for (let dx of [-1, 0, 1]) {
+                    for (let dy of [-1, 0, 1]) {
+                        if (dx === 0 && dy === 0) continue;
+                        const nx = (x + dx + currentGrid.cols) % currentGrid.cols;
+                        const ny = (y + dy + currentGrid.rows) % currentGrid.rows;
+                        if (currentGrid.getCellState(nx, ny) === 1) n++;
+                    }
+                }
+        
+                const state = currentGrid.getCellState(x, y);
+        
+                if (state === 1) {
+                    this.canvasGrid.fillCell(x, y, "#fff");
+                    if (n < 2 || n > 3) newGrid.setCellState(x, y, 0);
+                } else {
+                    this.canvasGrid.fillCell(x, y, "#000");
+                    if (n === 3) newGrid.setCellState(x, y, 1);
                 }
             }
-
-            const state = currentGrid.getCellState(x, y);
-
-            if (state === 1) {
-                this.canvasGrid.fillCell(x, y, "#fff");
-                if (n < 2 || n > 3) newGrid.setCellState(x, y, 0);
-            } else {
-                this.canvasGrid.fillCell(x, y, "#000");
-                if (n === 3) newGrid.setCellState(x, y, 1);
-            }
-        }
-        this.canvasGrid.cellGrid = newGrid;
-        this.canvasGrid.drawGridLines();
+            this.canvasGrid.cellGrid = newGrid;
+            this.canvasGrid.drawGridLines();
+        });
     }
 
     play() {
