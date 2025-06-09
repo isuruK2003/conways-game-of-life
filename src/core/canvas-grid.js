9cf960676128c0bdb5e7cb3008c678c8f1d11cb8
Isuru Karunaratna <93091809+isuruK2003@users.noreply.github.com>
2025-06-09 13:52:35 +0530

    refactor: update CanvasGrid to use onRenderFrame callback and improve rendering logic

--- src/core/canvas-grid.js
+++ src/core/canvas-grid.js
@@ -8,28 +8,24 @@ export class CanvasGrid {
         cellWidth,
         cellHeight,
         frameIntervalMillis = 100,
-        renderFrame = () => { }
+        onRenderFrame = () => { }
     ) {
-        this.cellGrid = new CellGrid(rows, cols, cellWidth, cellHeight);
         this.frameInterval = frameIntervalMillis;
-        this.renderFrame = renderFrame;
         this.lastUpdateTime = 0;
         this.animationId = null;
+        this.onRenderFrame = onRenderFrame;
         this.canvas = document.getElementById(canvasId);
+        this.cellGrid = new CellGrid(rows, cols, cellWidth, cellHeight);
         if (!this.canvas) {
             throw new Error("canvas element with canvas canvasId=" + canvasId + "can not be found");
         }
-        this.canvas.width = this.cellGrid.width;
-        this.canvas.height = this.cellGrid.height;
-        this.ctx = this.canvas.getContext("2d");
-
         const handleMouseEvent = (event) => {
             const x = Math.floor((event.x) / this.cellGrid.cellWidth);
             const y = Math.floor((event.y) / this.cellGrid.cellHeight);
             this.cellGrid.setCellState(x, y, 1);
             this.fillCell(x, y, "#fff");
+            !this.animationId && this.renderFrame({ sideEffects: false });
         }
-
         let isMouseDown = false;
         window.addEventListener("mousedown", () => {
             isMouseDown = true;
@@ -45,11 +41,11 @@ export class CanvasGrid {
                 handleMouseEvent(event);
             }
         });
-        this.drawGridLines();
-    }
-
-    setRenderFrame(renderFrame) {
-        this.renderFrame = renderFrame;
+        this.gridLinesEnabled = true;
+        this.canvas.width = this.cellGrid.width;
+        this.canvas.height = this.cellGrid.height;
+        this.ctx = this.canvas.getContext("2d");
+        this.renderGridLines();
     }
 
     fillCell(x, y, color) {
@@ -62,27 +58,39 @@ export class CanvasGrid {
         );
     }
 
-    drawGridLines() {
-        this.ctx.strokeStyle = "#444";
-        this.ctx.lineWidth = 0.5;
+    renderGridLines() {
+        if (this.gridLinesEnabled) {
+            this.ctx.strokeStyle = "#444";
+            this.ctx.lineWidth = 0.5;
 
-        for (let x = 0; x <= this.cellGrid.width; x += this.cellGrid.cellWidth) {
-            this.ctx.beginPath();
-            this.ctx.moveTo(x, 0);
-            this.ctx.lineTo(x, this.cellGrid.height);
-            this.ctx.stroke();
-        }
+            for (let x = 0; x <= this.cellGrid.width; x += this.cellGrid.cellWidth) {
+                this.ctx.beginPath();
+                this.ctx.moveTo(x, 0);
+                this.ctx.lineTo(x, this.cellGrid.height);
+                this.ctx.stroke();
+            }
 
-        for (let y = 0; y <= this.cellGrid.height; y += this.cellGrid.cellHeight) {
-            this.ctx.beginPath();
-            this.ctx.moveTo(0, y);
-            this.ctx.lineTo(this.cellGrid.width, y);
-            this.ctx.stroke();
+            for (let y = 0; y <= this.cellGrid.height; y += this.cellGrid.cellHeight) {
+                this.ctx.beginPath();
+                this.ctx.moveTo(0, y);
+                this.ctx.lineTo(this.cellGrid.width, y);
+                this.ctx.stroke();
+            }
         }
     }
 
-    clearCanvas() {
-        this.ctx.clearRect(0, 0, this.cellGrid.width, this.cellGrid.height);
+    renderFrame({ sideEffects = true } = {}) {
+        const currentGrid = this.cellGrid;
+        const bufferGrid = this.cellGrid.getClone();
+        for (let i = 0; i < currentGrid.rows * currentGrid.cols; i++) {
+            const x = i % currentGrid.cols;
+            const y = Math.floor(i / currentGrid.cols);
+            const color = currentGrid.getCellState(x, y) === 1 ? "#fff" : "#000";
+            this.fillCell(x, y, color);
+            sideEffects && this.onRenderFrame(x, y, currentGrid, bufferGrid);
+        }
+        this.cellGrid = bufferGrid;
+        this.renderGridLines();
     }
 
     render(timestamp) {
@@ -98,5 +106,9 @@ export class CanvasGrid {
             cancelAnimationFrame(this.animationId);
             this.animationId = null;
         }
+    }
+
+    clearCanvas() {
+        this.ctx.clearRect(0, 0, this.cellGrid.width, this.cellGrid.height);
     }
 }
--- src/core/game.js
+++ src/core/game.js
@@ -1,58 +1,43 @@
 export class GameOfLife {
     constructor(canvasGrid) {
-        this.canvasGrid = canvasGrid;
         this.isPlaying = false;
-        this.gridLinesEnabled = true;
-        this.canvasGrid.setRenderFrame(() => {
-            const currentGrid = this.canvasGrid.cellGrid;
-            const newGrid = this.canvasGrid.cellGrid.getClone();
-            for (let i = 0; i < currentGrid.rows * currentGrid.cols; i++) {
-                const x = i % currentGrid.cols;
-                const y = Math.floor(i / currentGrid.cols);
-
-                let n = 0;
-                for (let dx of [-1, 0, 1]) {
-                    for (let dy of [-1, 0, 1]) {
-                        if (dx === 0 && dy === 0) continue;
-                        const nx = (x + dx + currentGrid.cols) % currentGrid.cols;
-                        const ny = (y + dy + currentGrid.rows) % currentGrid.rows;
-                        if (currentGrid.getCellState(nx, ny) === 1) n++;
-                    }
-                }
-
-                const state = currentGrid.getCellState(x, y);
-
-                if (state === 1) {
-                    this.canvasGrid.fillCell(x, y, "#fff");
-                    if (n < 2 || n > 3) newGrid.setCellState(x, y, 0);
-                } else {
-                    this.canvasGrid.fillCell(x, y, "#000");
-                    if (n === 3) newGrid.setCellState(x, y, 1);
+        this.canvasGrid = canvasGrid;
+        this.canvasGrid.onRenderFrame = (x, y, currentGrid, bufferGrid) => {
+            let n = 0;
+            for (let dx of [-1, 0, 1]) {
+                for (let dy of [-1, 0, 1]) {
+                    if (dx === 0 && dy === 0) continue;
+                    const nx = (x + dx + currentGrid.cols) % currentGrid.cols;
+                    const ny = (y + dy + currentGrid.rows) % currentGrid.rows;
+                    if (currentGrid.getCellState(nx, ny) === 1) n++;
                 }
             }
-            this.canvasGrid.cellGrid = newGrid;
-            this.gridLinesEnabled && this.canvasGrid.drawGridLines();
-        });
+
+            const state = currentGrid.getCellState(x, y);
+
+            if (state === 1 && n < 2 || n > 3) {
+                bufferGrid.setCellState(x, y, 0);
+            } else if (n === 3) {
+                bufferGrid.setCellState(x, y, 1);
+            }
+        };
     }
 
     toggle() {
-        if (this.isPlaying) {
-            this.canvasGrid.cancelRender();
-        } else {
-            this.canvasGrid.render();
-        }
+        this.isPlaying ? this.canvasGrid.cancelRender() : this.canvasGrid.render();
         this.isPlaying = !this.isPlaying;
     }
 
     toggleGridLines() {
-        this.gridLinesEnabled = !this.gridLinesEnabled;
+        this.canvasGrid.gridLinesEnabled = !this.canvasGrid.gridLinesEnabled;
+        !this.isPlaying && this.canvasGrid.renderFrame({ sideEffects: false });
     }
 
     reset() {
         this.canvasGrid.cancelRender();
         this.canvasGrid.cellGrid.clearGrid();
         this.canvasGrid.clearCanvas();
-        this.gridLinesEnabled && this.canvasGrid.drawGridLines();
+        this.canvasGrid.renderGridLines();
         this.isPlaying = false;
     }
 }
